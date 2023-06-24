5e3bd7cee2f8b57587602f5f0c8a04f83916923a
selimdilsad <selimdilsadercan@gmail.com>
2023-06-24 23:03:08 +0300

    init 2

--- sanity.config.ts
+++ sanity.config.ts
@@ -3,7 +3,7 @@ import { deskTool } from 'sanity/desk';
 import schemas from "./sanity/schemas"
 
 const config = defineConfig({
-  projectId: "txd29ab2",
+  projectId: "qjhbstq4",
   dataset: "production",
   title: "My Personal Website",
   apiVersion: "2023-03-09",
